ebf43eb754d624b3a2f0ea256261b0d40a085109
Hector R. Salazar <hector.sa@gmail.com>
2013-08-30 16:11:50 -0500

    Commit Inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+H2cWebsite::Application.config.secret_key_base = '245be956240d6eccc360771b234c985f5505ab56d7200f1e053821b904f07c589a73b75ef52e5da85fc9744b0f4df1a47d069704ae261ff754aaaca23b26ba8d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+H2cWebsite::Application.config.session_store :cookie_store, key: '_h2c_website_session'
